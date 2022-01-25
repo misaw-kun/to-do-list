f5fcb12982fc63f102fb1032980672bb5f0e5fd7
wasim <w.akram.1912@protonmail.com>
2022-01-25 20:30:59 +0530

    init

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import UserInterface from "./modules/UserInterface";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+//loading the app
+document.addEventListener('DOMContentLoaded', UserInterface.load);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  devtool: 'inline-source-map',
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 9000,
+  },
+  mode: "development",
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'To-Do-List',
+      template: path.resolve(__dirname, './src/template.html'),
+      filename: 'index.html'
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+};
